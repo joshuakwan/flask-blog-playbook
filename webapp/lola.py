8fa73da76e7b3c78d0401f99ba65c74202e1c864
joshuakwan <garfilone@gmail.com>
2015-08-25 03:20:50 +0000

    Polish the playbook.yml a bit more

--- webapp/lola.py
+++ webapp/lola.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "<h1 style='color:blue'>The Ballad of Jo & Lo!</h1>"
+    return "<h1>The Ballad of Jo & Lo</h1>"
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0', port=8080)
